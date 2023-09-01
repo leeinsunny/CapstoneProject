0545866c94a45971ffb9cd827ce06ab3d66bab83
doorooful <dooroo.jeong@intel.com>
2023-09-01 18:20:02 +0900

    navigation bar implementation
    - Page routing function
    - Font rule added
    
    Signed-off-by: doorooful <dooroo.jeong@intel.com>

---
+++ dt-demo-fe/src/pages/01_home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Home = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        height: '100vh'
+      }}
+    >
+      <div>Hi this is 01_home.js</div>
+    </div>
+  );
+};
+  
+export default Home;
---
+++ dt-demo-fe/src/pages/04_statistics.js
@@ -0,0 +1,18 @@
+import React from 'react';
+  
+const Statistics = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        height: '100vh'
+      }}
+    >
+      <div>Hi this is 04_statistics.js</div>
+    </div>
+  );
+};
+  
+export default Statistics;
