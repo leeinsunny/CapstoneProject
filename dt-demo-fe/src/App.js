5eb09b2055cc51e864eb71d809f5aabcd050faa4
yoouung <jyp101400@gmail.com>
2023-09-08 15:24:29 +0900

    style settings - add layout & font rules in reset.js - delete css files

--- dt-demo-fe/src/App.js
+++ dt-demo-fe/src/App.js
@@ -1,26 +1,40 @@
 // import logo from './logo.svg';
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/01_home';
-import Filter from './pages/02_filter';
-import Converter from './pages/03_converter';
-import Statistics from './pages/04_statistics';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import { Global } from "@emotion/react";
+import { reset } from "./styles/reset";
+import Home from "./pages/01_home";
+import Filter from "./pages/02_filter";
+import Converter from "./pages/03_converter";
+import Statistics from "./pages/04_statistics";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Navigate replace to="/home" />} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/filter' element={<Filter/>} />
-        <Route path='/converter' element={<Converter/>} />
-        <Route path='/statistics' element={<Statistics/>} />
-      </Routes>
-  </Router>
-  );
+    return (
+        <>
+            <Global styles={reset} />
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={<Navigate replace to="/home" />}
+                    />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/filter" element={<Filter />} />
+                    <Route path="/converter" element={<Converter />} />
+                    <Route path="/statistics" element={<Statistics />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
