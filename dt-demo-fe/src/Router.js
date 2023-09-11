89151ca5929d0a8effba4e0cd273e1ab0480707f
yoouung <jyp101400@gmail.com>
2023-09-11 17:36:02 +0900

    router bug fix

--- dt-demo-fe/src/Router.js
+++ dt-demo-fe/src/Router.js
@@ -4,32 +4,26 @@ import Home from "./pages/01_home";
 import Filter from "./pages/02_filter";
 import Converter from "./pages/03_converter";
 import Statistics from "./pages/04_statistics";
-import DemoUpload from "./pages/demo/upload";
 import DemoFileList from "./pages/demo/filelist";
 import DemoRules from "./pages/demo/rules";
 import DemoReport from "./pages/demo/report";
+import DemoUpload from "./pages/demo/upload";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Navbar />
-            {/* <Routes>
-                <Route
-                    exact
-                    path="/"
-                    element={<Navigate replace to="/home" />}
-                />
+            <Routes>
+                <Route path="/" element={<Navigate replace to="/home" />} />
                 <Route path="/home" element={<Home />} />
-                <Route path="/filter" element={<Filter />} />
+                <Route path="/filter" element={<Filter />}>
+                    <Route index element={<DemoUpload />} />
+                    <Route path="filelist" element={<DemoFileList />} />
+                    <Route path="rules" element={<DemoRules />} />
+                    <Route path="report" element={<DemoReport />} />
+                </Route>
                 <Route path="/converter" element={<Converter />} />
                 <Route path="/statistics" element={<Statistics />} />
-            </Routes> */}
-            <Routes>
-                <Route path="/" element={<Navigate replace to="/upload" />} />
-                <Route path="upload" element={<DemoUpload />} />
-                <Route path="filelist" element={<DemoFileList />} />
-                <Route path="rules" element={<DemoRules />} />
-                <Route path="report" element={<DemoReport />} />
             </Routes>
         </BrowserRouter>
     );
